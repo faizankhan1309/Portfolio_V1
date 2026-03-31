fefc47badd462448a93ae39d6645db89c0267069
faizankhan1309 <khan.faizan139@gmail.com>
2026-03-31 15:52:34 +0530

    update navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { Menu, X } from 'lucide-react';
-import { Home } from "lucide-react";
+
 
 const navLinks = [
   { name: 'About', href: '#Hero' },
